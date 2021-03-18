cf79f281f1ef0bb0b6636cf1804d788a8ae0c89a
taibuharoub <haroubtaibu@gmail.com>
2021-03-18 07:06:02 +0300

    Error handling Middleware

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
+if(process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
+
 const express = require("express");
 
-const app = express()
-const PORT = 3000
+const ExpressError = require("./utils/ExpressError");
 
-app.set("view engine", "ejs")
+const app = express();
+const PORT = 3000;
 
-app.get("/", (req, res, next) =>{
-    res.render("index")
-})
+app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong";
+  res.status(statusCode).render("error", { err });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server Started at http://localhost:${PORT}`);
-})
+  console.log(`Server Started at http://localhost:${PORT}`);
+});
